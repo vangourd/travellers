8bc044852436dcdb189358302addbbb7a988b173
Brian Logan <id@brianjlogan.com>
2020-04-03 12:02:18 -0400

    Renamed UI, refactored logic into Board and Tile classes, added Logic for resource tile limits with recursion

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import sys, random
 from PySide2.QtWidgets import QDialog, QApplication
-from board import *
+from game import *
 
-class MyForm(QDialog):
+class Board:
 
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
-        self.ui.tileOptions = [
+    def __init__(self,places):
+        self.tiles = []
+        self.generateTiles(places)
+    
+    def generateTiles(self,places):
+        for place in places:
+            self.tiles.append(Tile(place))
+
+class Tile:
+    options = [
             {"name":"aura","path":"img/aura.svg","used":0},
             {"name":"gas","path":"img/gas.svg","used":0},
             {"name":"nova","path":"img/nova.svg","used":0},
@@ -16,7 +21,41 @@ class MyForm(QDialog):
             {"name":"steel","path":"img/steel.svg","used":0},
             {"name":"asteroids","path":"img/asteroids.svg","used":0}
         ]
-        self.ui.tiles = [
+
+    def __init__(self, ui):
+        self.ui = ui
+        self.name = None
+        self.type = None
+        self.value = 0
+        if ui.objectName() == "tile_10":
+            self.setDesert()
+        else:
+            self.differentiate()
+
+    def differentiate(self):
+        choice = random.randrange(5)
+        if self.options[choice]['used'] < 4:
+            decision = self.options[choice]
+            self.type = decision['name']
+            self.name = self.ui.objectName()
+            self.ui.setPixmap(QPixmap(decision['path']))
+            self.options[choice]['used'] += 1
+        else:
+            self.differentiate()
+
+    def setDesert(self):
+        decision = self.options[5]
+        self.type = decision['name']
+        self.ui.setPixmap(QPixmap(decision['path']))
+
+class MyForm(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.gameStatus = "playing"
+        self.ui.places = [
             self.ui.tile_01,
             self.ui.tile_02,
             self.ui.tile_03,
@@ -37,17 +76,12 @@ class MyForm(QDialog):
             self.ui.tile_18,
             self.ui.tile_19,
         ]
-        for tile in self.ui.tiles:
-            if tile.objectName() == "tile_10":
-                tile.setPixmap(QPixmap(self.ui.tileOptions[5]['path']))
-                print(tile)
-                continue
-            choice = random.randrange(5)
-            current_tile = self.ui.tileOptions[choice]
-            tile.setPixmap(QPixmap(current_tile['path']))
-            current_tile['used'] += 1
-            
+        self.ui.startGameButton.clicked.connect(self.new_game)
         self.show()
+    
+    def new_game(self):
+        self.ui.startGameButton.setHidden(True)
+        self.board = Board(self.ui.places)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
